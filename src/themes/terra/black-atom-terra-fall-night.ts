3735c859bdf3eb42dd52d3d4fa9ab09eb25798e6
nbr <nikolaus.brunner@protonmail.ch>
2025-10-15 18:12:19 +0200

    refactor(themes/terra): normalize terra fall night color primaries

--- src/themes/terra/black-atom-terra-fall-night.ts
+++ src/themes/terra/black-atom-terra-fall-night.ts
@@ -16,28 +16,28 @@ const meta: Theme.Meta = {
 };
 
 const primaries: Theme.Primaries = {
-    d10: oklch(0.243, 0.025, 58.11),
-    d20: oklch(0.262, 0.03, 46.15),
-    d30: oklch(0.302, 0.03, 49.08),
-    d40: oklch(0.339, 0.03, 47.8),
+    d10: oklch(0.18, 0.025, 50.0),
+    d20: oklch(0.22, 0.025, 50.0),
+    d30: oklch(0.26, 0.025, 50.0),
+    d40: oklch(0.30, 0.025, 50.0),
 
-    m10: oklch(0.483, 0.075, 48.75),
-    m20: oklch(0.569, 0.074, 49.6),
-    m30: oklch(0.655, 0.075, 48.42),
-    m40: oklch(0.742, 0.075, 49.01),
+    m10: oklch(0.50, 0.075, 50.0),
+    m20: oklch(0.60, 0.075, 50.0),
+    m30: oklch(0.65, 0.075, 50.0),
+    m40: oklch(0.70, 0.075, 50.0),
 
-    l10: oklch(0.784, 0.05, 49.4),
-    l20: oklch(0.85, 0.05, 50.05),
-    l30: oklch(0.891, 0.05, 48.14),
-    l40: oklch(0.935, 0.036, 48.31),
+    l10: oklch(0.75, 0.05, 50.0),
+    l20: oklch(0.80, 0.05, 50.0),
+    l30: oklch(0.85, 0.05, 50.0),
+    l40: oklch(0.90, 0.0439, 50.0),
 };
 
 const palette: Theme.Palette = {
     black: primaries.d30,
     gray: primaries.m10,
 
-    darkRed: oklch(0.656, 0.14, 29.42),
-    red: oklch(0.699, 0.14, 29.58),
+    darkRed: oklch(0.6, 0.14, 30),
+    red: oklch(0.7, 0.14, 30),
 
     darkGreen: oklch(0.655, 0.05, 127.63),
     green: oklch(0.698, 0.05, 127.55),
